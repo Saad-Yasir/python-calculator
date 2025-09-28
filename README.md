bff5e04488d19c2c774050f68eda449f274affca
Saad-Yasir <mian.saadyasir@gmail.com>
2025-09-28 16:55:18 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+Python Calculator:
+A simple calculator program written in python. Supports addition (+), subtraction (-), multiplication (*), division (/), and power (**).
+
+To run the program, you'll have to enter a number, an operator (+, -, *, /, or **), and a second number.
+
+
---
+++ calculator.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 28 16:21:19 2025
+
+@author: Lenovo
+"""
+
+def add(x,y):
+    return x+y
+
+def subtract(x,y):
+    return x-y
+
+def divide(x,y):
+    return x/y
+
+def multiply(x,y):
+    return x*y
+
+def power(x,y):
+    return x**y
+
+print("Calculator Operations: +, -, *, /, **")
+num1=float(input("Enter the first number: "))
+op=input("Enter the operator +, -, *, / or **: ")
+num2=float(input("Enter the second number: "))
+
+if op == "+":
+    print("Result: ", add(num1,num2))
+elif op == "-":
+    print("Result: ", subtract(num1,num2))
+elif op == "*":
+    print("Result: ", multiply(num1,num2))
+elif op=="/":
+    if num2==0:
+        print("Error: Division by zero")
+    else:
+        print("Result: ", divide(num1,num2))
+elif op=="**":
+    print("Result: ", power(num1,num2))
+else:
+    print("Invalid operation")
+    
+        
+    
+    
+    
